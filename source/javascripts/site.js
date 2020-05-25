d1641c18915396db09f2f9685ac36c5b181ed5a4
Amber Aultman <amber.aultman@gmail.com>
2020-05-25 00:48:06 -0700

    Adds ionicons and some basic js styling for about

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,11 @@
-$( "#date" ).datepicker();
+// $( "#date" ).datepicker();
+
+// Expands About section on homepage
+function showHide() {
+    let about = document.getElementById("about");
+    if (about.style.display === "none") {
+      about.style.display = "grid";
+    } else {
+      about.style.display = "none";
+    }
+  }
