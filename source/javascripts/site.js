79b4792743ba916ff1675d301960f84e18ea6375
Amber Aultman <amber.aultman@gmail.com>
2020-05-25 19:19:27 -0700

    Removes jQuery, fixes grid and flexbox issues courtesy of Colin :)

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,11 +1,15 @@
-// $( "#date" ).datepicker();
 
 // Expands About section on homepage
-function showHide() {
+// function showHide() {
+//     let about = document.getElementById("about");
+//     if (about.style.display === "none") {
+//       about.style.display = "grid";
+//     } else {
+//       about.style.display = "none";
+//     }
+//   }
+
+  function showHide() {
     let about = document.getElementById("about");
-    if (about.style.display === "none") {
-      about.style.display = "grid";
-    } else {
-      about.style.display = "none";
-    }
+    about.classList.toggle("slide-open")
   }
