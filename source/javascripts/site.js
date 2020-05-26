cf90b77a6c085bfcee997098955ba1aba8c989bf
Colin Darland <colin@tn.com>
2020-05-25 20:52:21 -0700

    refactor: Updates some DOM nodes, styles and JS in service of the homepage about section

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,15 +1,6 @@
-
 // Expands About section on homepage
-// function showHide() {
-//     let about = document.getElementById("about");
-//     if (about.style.display === "none") {
-//       about.style.display = "grid";
-//     } else {
-//       about.style.display = "none";
-//     }
-//   }
 
-  function showHide() {
-    let about = document.getElementById("about");
-    about.classList.toggle("slide-open")
-  }
+function showHide() {
+  let about = document.getElementById("about");
+  about.classList.toggle("slide")
+}
