aa1ec16ed4cdd352c7499adfbcd9aa1059cf4ad5
Amber Aultman <amber.aultman@gmail.com>
2020-05-29 12:50:57 -0700

    Adds Tiny Slider and starts adjusting styles

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -4,3 +4,55 @@ function showHide() {
   let about = document.getElementById("about");
   about.classList.toggle("slide")
 }
+
+// Tiny Slider modifiers
+
+var slider = tns({
+  container: '#slider-work',
+  loop: false,
+  items: 1,
+  navPosition: 'bottom',
+  gutter: 50,
+  // lazyload: true,
+  // lazyloadSelector: '.tns-lazy',
+  mouseDrag: true,
+  // edgePadding: 20,
+  responsive: {
+    480: {
+      // edgePadding: 100,
+      // gutter: 20,
+      items: 2,
+      slideBy: 2
+    },
+    768: {
+      // gutter: 30,
+      items: 3.3,
+      slideBy: 3
+    },
+  }
+});
+
+var slider = tns({
+  container: '#slider-experience',
+  loop: false,
+  items: 1,
+  navPosition: 'bottom',
+  gutter: 50,
+  // lazyload: true,
+  // lazyloadSelector: '.tns-lazy',
+  mouseDrag: true,
+  // edgePadding: 20,
+  responsive: {
+    480: {
+      // edgePadding: 100,
+      // gutter: 20,
+      items: 2,
+      slideBy: 2
+    },
+    768: {
+      // gutter: 30,
+      items: 3.3,
+      slideBy: 3
+    },
+  }
+});
