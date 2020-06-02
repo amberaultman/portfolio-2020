8bf37cebb2363cf30570a153548e4bb7ab0389d6
Amber Aultman <amber.aultman@gmail.com>
2020-06-01 22:06:00 -0700

    Styles Tiny Slider JS and associated controls

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -12,47 +12,61 @@ var slider = tns({
   loop: false,
   items: 1,
   navPosition: 'bottom',
-  gutter: 50,
-  // lazyload: true,
-  // lazyloadSelector: '.tns-lazy',
+  gutter: 0,
+  edgePadding: 20,
+  speed: 600,
+  slideBy: 'page',
   mouseDrag: true,
-  // edgePadding: 20,
+  arrowKeys: true,
+  nav: false,
+  controlsText: ['<ion-icon name="arrow-back-outline"></ion-icon>', '<ion-icon name="arrow-forward-outline"></ion-icon>'],
+  controlsContainer: '.controls',
   responsive: {
     480: {
-      // edgePadding: 100,
-      // gutter: 20,
       items: 2,
-      slideBy: 2
+      gutter: 20,
+      edgePadding: 40
     },
     768: {
-      // gutter: 30,
-      items: 3.3,
-      slideBy: 3
+      items: 3,
+      gutter: 30,
+      edgePadding: 60
     },
+    1200: {
+      items: 4,
+      gutter: 40,
+      edgePadding: 80
+    },
+    1920: {
+      items: 5,
+      gutter: 50,
+      edgePadding: 100
+    }
   }
 });
 
-var slider = tns({
-  container: '#slider-experience',
-  loop: false,
-  items: 1,
-  navPosition: 'bottom',
-  gutter: 50,
+// var slider = tns({
+//   container: '#slider-experience',
+//   loop: false,
+//   items: 1,
+//   navPosition: 'bottom',
+//   gutter: 50,
+//   slideBy: 'page',
   // lazyload: true,
   // lazyloadSelector: '.tns-lazy',
-  mouseDrag: true,
+  // mouseDrag: true,
   // edgePadding: 20,
-  responsive: {
-    480: {
-      // edgePadding: 100,
-      // gutter: 20,
-      items: 2,
-      slideBy: 2
-    },
-    768: {
-      // gutter: 30,
-      items: 3.3,
-      slideBy: 3
-    },
-  }
-});
+  // responsive: {
+  //   480: {
+  //     edgePadding: 100,
+  //     gutter: 20,
+  //     items: 2,
+  //     slideBy: 2
+  //   },
+  //   768: {
+  //     gutter: 30,
+  //     edgePadding: 50,
+  //     items: 3,
+  //   },
+  // }
+// });
