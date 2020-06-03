81a1fbd8bece9f25d29140f222940a0f0a559b6d
Amber Aultman <amber.aultman@gmail.com>
2020-06-02 21:34:35 -0700

    Fixes Tiny Slider and disabled button hover states

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -12,13 +12,14 @@ var slider = tns({
   loop: false,
   items: 1,
   navPosition: 'bottom',
-  gutter: 0,
+  gutter: 10,
   edgePadding: 20,
   speed: 600,
   slideBy: 'page',
   mouseDrag: true,
   arrowKeys: true,
   nav: false,
+  autoHeight: false,
   controlsText: ['<ion-icon name="arrow-back-outline"></ion-icon>', '<ion-icon name="arrow-forward-outline"></ion-icon>'],
   controlsContainer: '.controls',
   responsive: {
@@ -45,28 +46,41 @@ var slider = tns({
   }
 });
 
-// var slider = tns({
-//   container: '#slider-experience',
-//   loop: false,
-//   items: 1,
-//   navPosition: 'bottom',
-//   gutter: 50,
-//   slideBy: 'page',
-  // lazyload: true,
-  // lazyloadSelector: '.tns-lazy',
-  // mouseDrag: true,
-  // edgePadding: 20,
-  // responsive: {
-  //   480: {
-  //     edgePadding: 100,
-  //     gutter: 20,
-  //     items: 2,
-  //     slideBy: 2
-  //   },
-  //   768: {
-  //     gutter: 30,
-  //     edgePadding: 50,
-  //     items: 3,
-  //   },
-  // }
-// });
+var slider = tns({
+  container: '#slider-experience',
+  loop: false,
+  items: 1,
+  navPosition: 'bottom',
+  gutter: 10,
+  edgePadding: 20,
+  speed: 600,
+  slideBy: 'page',
+  mouseDrag: true,
+  arrowKeys: true,
+  nav: false,
+  autoHeight: false,
+  controlsText: ['<ion-icon name="arrow-back-outline"></ion-icon>', '<ion-icon name="arrow-forward-outline"></ion-icon>'],
+  controlsContainer: '.controls',
+  responsive: {
+    480: {
+      items: 2,
+      gutter: 20,
+      edgePadding: 40
+    },
+    768: {
+      items: 3,
+      gutter: 30,
+      edgePadding: 60
+    },
+    1200: {
+      items: 4,
+      gutter: 40,
+      edgePadding: 80
+    },
+    1920: {
+      items: 5,
+      gutter: 50,
+      edgePadding: 100
+    }
+  }
+});
