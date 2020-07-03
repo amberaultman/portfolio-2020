cbc7250644034a5299118bd9e26817f0aaf392b7
Amber Aultman <amber.aultman@gmail.com>
2020-07-03 16:25:44 -0700

    Fixs smooth scroll js, adds responsive styles to case studies

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -7,27 +7,37 @@ function showHide() {
   rotateIcon.classList.toggle("rotate");
 }
 
-// Smooth scrolling
 
-function smoothScroll() {
-  document.querySelector('#top').scrollIntoView({ 
-    behavior: 'smooth' 
-  });
+// Vanilla JavaScript Scroll to Anchor
+// @ https://perishablepress.com/vanilla-javascript-scroll-anchor/
+
+(function() {
+	scrollTo();
+})();
+
+function scrollTo() {
+	const links = document.querySelectorAll('.scroll');
+	links.forEach(each => (each.onclick = scrollAnchors));
 }
 
-// function smoothScroll() {
-//   let smoothly = document.querySelector('#top').scrollIntoView({ 
-//     behavior: 'smooth' 
-//   });
-//   return smoothly;
-// }
-
-// function smoothScroll() {
-//   let smoothly = document.getElementsByClassName("smoothly");
-//   smoothly.document.querySelector('#top').scrollIntoView({ 
-//     behavior: 'smooth' 
-//   });
-// }
+function scrollAnchors(e, respond = null) {
+	const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
+	e.preventDefault();
+	var targetID = (respond) ? respond.getAttribute('href') : this.getAttribute('href');
+	const targetAnchor = document.querySelector(targetID);
+	if (!targetAnchor) return;
+	const originalTop = distanceToTop(targetAnchor);
+	window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
+	const checkIfDone = setInterval(function() {
+		const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
+		if (distanceToTop(targetAnchor) === 0 || atBottom) {
+			targetAnchor.tabIndex = '-1';
+			targetAnchor.focus();
+			window.history.pushState('', '', targetID);
+			clearInterval(checkIfDone);
+		}
+	}, 100);
+}
 
 
 // Tiny Slider modifiers
