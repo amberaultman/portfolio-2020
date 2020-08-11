a6de57937f2374188c0d46ed4c958b9863128212
Amber Aultman <amber.aultman@gmail.com>
2020-08-11 16:59:27 -0700

    Add build files to docs folder

--- config.rb
+++ config.rb
@@ -45,13 +45,13 @@ page '/*.txt', layout: false
 #   activate :minify_javascript
 # end
 
-activate :deploy do |deploy|
-  deploy.deploy_method = :git
-  deploy.branch = 'master'
-  deploy.build_before = true
-  # Optional Settings
-  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
-  # deploy.branch   = 'custom-branch' # default: gh-pages
-  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
-  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
-end
+# activate :deploy do |deploy|
+#   deploy.deploy_method = :git
+#   deploy.branch = 'master'
+#   deploy.build_before = true
+#   # Optional Settings
+#   # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+#   # deploy.branch   = 'custom-branch' # default: gh-pages
+#   # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+#   # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+# end
